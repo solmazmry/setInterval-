50a6160e20b4477e792b4d36bd0988c977dc8e63
Solmazmry <167036192+solmazmry@users.noreply.github.com>
2024-06-06 11:28:35 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+// Ekrana sehife açılandan sonra consola-a Hello world cixsin
+// let count= 'Hello World'
+// const consola= ()=>{
+// setTimeout (()=>{
+//     console.log(count);
+// },200)
+// }
+// consola()
+
+// Counter = 0.her 5 saniyeden bir counter 1 vahid artsin,10olarsa dayansin
+// function CountDown() {
+//   count = 0;
+//   let intervals = setInterval(() => {
+// console.log(count);
+// count ++;
+//   if(count>10){
+//     clearInterval(intervals)
+//   }
+//   },5000);
+// }
+// CountDown()
